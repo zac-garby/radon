d780feb52623fb142ef7db7f974d1fd072ac92ed
Zac Garby <zacgarby@icloud.com>
2017-11-20 16:25:43 +0000

    Steal lexer from Pluto, since syntax is similar

---
+++ lexer/lexemes.go
@@ -0,0 +1,104 @@
+package lexer
+
+import (
+	"strings"
+
+	"github.com/Zac-Garby/pluto/token"
+)
+
+type transformer func(token.Type, string, string) (token.Type, string, string)
+type handler func([]string) (token.Type, string, string)
+
+func lexemeHandler(t token.Type, group int, transformer transformer) handler {
+	return func(m []string) (token.Type, string, string) {
+		return transformer(t, m[group], m[0])
+	}
+}
+
+func none(t token.Type, literal, whole string) (token.Type, string, string) {
+	return t, literal, whole
+}
+
+func stringTransformer(t token.Type, literal, whole string) (token.Type, string, string) {
+	escapes := map[string]string{
+		`\n`: "\n",
+		`"`:  "\"",
+		`\a`: "\a",
+		`\b`: "\b",
+		`\f`: "\f",
+		`\r`: "\r",
+		`\t`: "\t",
+		`\v`: "\v",
+	}
+
+	for k, v := range escapes {
+		literal = strings.Replace(literal, k, v, -1)
+	}
+
+	return t, literal, whole
+}
+
+func idTransformer(t token.Type, literal, whole string) (token.Type, string, string) {
+	if t, ok := token.Keywords[literal]; ok {
+		return t, literal, whole
+	}
+
+	return t, literal, whole
+}
+
+type lexicalPair struct {
+	regex   string
+	handler handler
+}
+
+var lexicalDictionary = []lexicalPair{
+	// Literals
+	{regex: `^\d+(?:\.\d+)?`, handler: lexemeHandler(token.Number, 0, none)},
+	{regex: `^"((\\"|[^"])*)"`, handler: lexemeHandler(token.String, 1, stringTransformer)},
+	{regex: "^`([^`]*)`", handler: lexemeHandler(token.String, 1, none)},
+	{regex: `^\w+`, handler: lexemeHandler(token.ID, 0, idTransformer)},
+
+	// Punctuation
+	{regex: `^->`, handler: lexemeHandler(token.Arrow, 0, none)},
+	{regex: `^\+=`, handler: lexemeHandler(token.PlusEquals, 0, none)},
+	{regex: `^\+`, handler: lexemeHandler(token.Plus, 0, none)},
+	{regex: `^-=`, handler: lexemeHandler(token.MinusEquals, 0, none)},
+	{regex: `^-`, handler: lexemeHandler(token.Minus, 0, none)},
+	{regex: `^\*\*=`, handler: lexemeHandler(token.ExpEquals, 0, none)},
+	{regex: `^\*\*`, handler: lexemeHandler(token.Exp, 0, none)},
+	{regex: `^\*=`, handler: lexemeHandler(token.StarEquals, 0, none)},
+	{regex: `^\*`, handler: lexemeHandler(token.Star, 0, none)},
+	{regex: `^\/\/=`, handler: lexemeHandler(token.FloorDivEquals, 0, none)},
+	{regex: `^\/\/`, handler: lexemeHandler(token.FloorDiv, 0, none)},
+	{regex: `^\/=`, handler: lexemeHandler(token.SlashEquals, 0, none)},
+	{regex: `^\/`, handler: lexemeHandler(token.Slash, 0, none)},
+	{regex: `^\(`, handler: lexemeHandler(token.LeftParen, 0, none)},
+	{regex: `^\)`, handler: lexemeHandler(token.RightParen, 0, none)},
+	{regex: `^<=`, handler: lexemeHandler(token.LessThanEq, 0, none)},
+	{regex: `^>=`, handler: lexemeHandler(token.GreaterThanEq, 0, none)},
+	{regex: `^<`, handler: lexemeHandler(token.LessThan, 0, none)},
+	{regex: `^>`, handler: lexemeHandler(token.GreaterThan, 0, none)},
+	{regex: `^{`, handler: lexemeHandler(token.LeftBrace, 0, none)},
+	{regex: `^}`, handler: lexemeHandler(token.RightBrace, 0, none)},
+	{regex: `^\[`, handler: lexemeHandler(token.LeftSquare, 0, none)},
+	{regex: `^]`, handler: lexemeHandler(token.RightSquare, 0, none)},
+	{regex: `^;`, handler: lexemeHandler(token.Semi, 0, none)},
+	{regex: `^==`, handler: lexemeHandler(token.Equal, 0, none)},
+	{regex: `^!=`, handler: lexemeHandler(token.NotEqual, 0, none)},
+	{regex: `^\|\|=`, handler: lexemeHandler(token.OrEquals, 0, none)},
+	{regex: `^\|\|`, handler: lexemeHandler(token.Or, 0, none)},
+	{regex: `^&&=`, handler: lexemeHandler(token.AndEquals, 0, none)},
+	{regex: `^&&`, handler: lexemeHandler(token.And, 0, none)},
+	{regex: `^\|=`, handler: lexemeHandler(token.BitOrEquals, 0, none)},
+	{regex: `^\|`, handler: lexemeHandler(token.BitOr, 0, none)},
+	{regex: `^&=`, handler: lexemeHandler(token.BitAndEquals, 0, none)},
+	{regex: `^&`, handler: lexemeHandler(token.BitAnd, 0, none)},
+	{regex: `^=`, handler: lexemeHandler(token.Assign, 0, none)},
+	{regex: `^:=`, handler: lexemeHandler(token.Declare, 0, none)},
+	{regex: `^\,`, handler: lexemeHandler(token.Comma, 0, none)},
+	{regex: `^:`, handler: lexemeHandler(token.Colon, 0, none)},
+	{regex: `^%=`, handler: lexemeHandler(token.ModEquals, 0, none)},
+	{regex: `^%`, handler: lexemeHandler(token.Mod, 0, none)},
+	{regex: `^\.`, handler: lexemeHandler(token.Dot, 0, none)},
+	{regex: `^!`, handler: lexemeHandler(token.Bang, 0, none)},
+}
---
+++ lexer/lexer.go
@@ -0,0 +1,154 @@
+package lexer
+
+import (
+	"regexp"
+	"unicode"
+
+	"github.com/Zac-Garby/pluto/token"
+)
+
+var lineEndings = []token.Type{
+	token.ID,
+	token.String,
+	token.Char,
+	token.Number,
+	token.True,
+	token.False,
+	token.Null,
+	token.Param,
+	token.Break,
+	token.Next,
+	token.Return,
+	token.RightParen,
+	token.RightSquare,
+	token.RightBrace,
+}
+
+// Lexer takes a string and returns a stream of tokens
+// The stream of tokens is in the form of a function
+// which returns the next token.
+func Lexer(str string) func() token.Token {
+	var (
+		index = 0
+		col   = 1
+		line  = 1
+		ch    = make(chan token.Token)
+	)
+
+	go func() {
+		for {
+			if index < len(str) {
+				foundSpace := false
+
+				for index < len(str) && (unicode.IsSpace(rune(str[index])) || str[index] == '#') {
+					if unicode.IsSpace(rune(str[index])) {
+						index++
+						col++
+
+						if str[index-1] == '\n' {
+							col = 1
+							line++
+						}
+
+						foundSpace = true
+					} else {
+						for index < len(str) && str[index] != '\n' {
+							index++
+						}
+
+						col = 1
+					}
+				}
+
+				if foundSpace {
+					continue
+				}
+
+				found := false
+
+				remainingSubstring := str[index:]
+
+				for _, pair := range lexicalDictionary {
+					var (
+						regex   = pair.regex
+						handler = pair.handler
+						pattern = regexp.MustCompile(regex)
+						match   = pattern.FindStringSubmatch(remainingSubstring)
+					)
+
+					if len(match) > 0 {
+						found = true
+						t, literal, whole := handler(match)
+						l := len(whole)
+
+						ch <- token.Token{
+							Type:    t,
+							Literal: literal,
+							Start:   token.Position{Line: line, Column: col},
+							End:     token.Position{Line: line, Column: col + l - 1},
+						}
+
+						index += l
+						col += l
+
+						for index < len(str) && unicode.IsSpace(rune(str[index])) && str[index] != '\n' {
+							index++
+							col++
+						}
+
+						if index < len(str) && str[index] == '#' {
+							for index < len(str) && str[index] != '\n' {
+								index++
+							}
+						}
+
+						isLineEnding := false
+
+						for _, ending := range lineEndings {
+							if t == ending {
+								isLineEnding = true
+							}
+						}
+
+						if (isLineEnding && index < len(str) && (str[index] == '\n' || str[index] == '}')) || index >= len(str) {
+							ch <- token.Token{
+								Type:    token.Semi,
+								Literal: ";",
+								Start:   token.Position{Line: line, Column: col},
+								End:     token.Position{Line: line, Column: col},
+							}
+						}
+
+						break
+					}
+				}
+
+				if !found {
+					ch <- token.Token{
+						Type:    token.Illegal,
+						Literal: string(str[index]),
+						Start:   token.Position{Line: line, Column: col},
+						End:     token.Position{Line: line, Column: col},
+					}
+
+					index++
+					col++
+				}
+			} else {
+				index++
+				col++
+
+				ch <- token.Token{
+					Type:    token.EOF,
+					Literal: "",
+					Start:   token.Position{Line: line, Column: col},
+					End:     token.Position{Line: line, Column: col},
+				}
+			}
+		}
+	}()
+
+	return func() token.Token {
+		return <-ch
+	}
+}
