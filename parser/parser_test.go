e375176ee54af1e47a94a620553cac3e88dbfab4
Zac Garby <zacgarby@icloud.com>
2018-01-08 11:24:15 +0000

    Test infix lambda parsing

--- parser/parser_test.go
+++ parser/parser_test.go
@@ -40,6 +40,8 @@ func TestErrors(t *testing.T) {
 
 		"import",
 		"import 5",
+
+		"5 => a",
 	}
 
 	for _, test := range tests {
@@ -167,6 +169,11 @@ func TestNoErrors(t *testing.T) {
 		"model(x, y,)",
 
 		"import 'hello'",
+
+		"a => b",
+		"() => b",
+		"(a) => b",
+		"(a, b) => c",
 	}
 
 	for _, test := range tests {
