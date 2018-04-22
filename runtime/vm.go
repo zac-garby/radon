b61961ec14d96ffb0c928229879f05e65de36f7b
Zac Garby <me@zacgarby.co.uk>
2018-04-22 11:59:22 +0100

    Define a few methods in the VM

--- runtime/vm.go
+++ runtime/vm.go
@@ -4,6 +4,7 @@ import (
 	"io"
 	"os"
 
+	"github.com/Zac-Garby/radon/bytecode"
 	"github.com/Zac-Garby/radon/object"
 )
 
@@ -41,4 +42,50 @@ func New() *VM {
 		Interrupts: make(chan Interrupt, InterruptQueueSize),
 		Out:        os.Stdout,
 	}
+}
+
+// makeFrame makes a Frame instance with the given parameters. offset is set to 0 and a new
+// data stack is created. The variables in args, if it's non-nil, are declared in the Frame's
+// store (note: declared, not assigned).
+func (v *VM) makeFrame(code bytecode.Code, args, store *Store, constants []object.Object, names []string) *Frame {
+	frame := &Frame{
+		code:      code,
+		store:     store,
+		offset:    0,
+		stack:     NewStack(),
+		constants: constants,
+		names:     names,
+		vm:        v,
+	}
+
+	if args != nil {
+		for k, v := range args.Data {
+			frame.store.Set(k, v.Value, true)
+		}
+	}
+
+	return frame
+}
+
+func (v *VM) pushFrame(frame *Frame) {
+	v.frames = append(v.frames, frame)
+}
+
+func (v *VM) popFrame() *Frame {
+	f := v.frames[len(v.frames)-1]
+	v.frames = v.frames[:len(v.frames)-1]
+	return f
+}
+
+// ExtractValue returns the top value from the top frame, if both those things exist.
+func (v *VM) ExtractValue() (object.Object, error) {
+	if len(v.frames) < 1 || v.frames[0].stack.Len() < 1 {
+		return nil, nil
+	}
+	return v.frames[0].stack.Top()
+}
+
+// Error returns the VM's error, if one exists. nil if there is no error.
+func (v *VM) Error() error {
+	return v.err
 }
