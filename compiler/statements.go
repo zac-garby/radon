f83f06df743a3bc5597a129fe307011e765c6c03
Zac Garby <me@zacgarby.co.uk>
2018-05-27 11:39:25 +0100

    Compile return statements

--- compiler/statements.go
+++ compiler/statements.go
@@ -5,6 +5,7 @@ import (
 	"reflect"
 
 	"github.com/Zac-Garby/radon/ast"
+	"github.com/Zac-Garby/radon/bytecode"
 )
 
 // CompileStatement takes an AST statement and generates some bytecode for it.
@@ -12,7 +13,19 @@ func (c *Compiler) CompileStatement(s ast.Statement) error {
 	switch node := s.(type) {
 	case *ast.ExpressionStatement:
 		return c.CompileExpression(node.Expr)
+	case *ast.Return:
+		return c.compileReturn(node)
 	default:
 		return fmt.Errorf("compiler: compilation not yet implemented for %s", reflect.TypeOf(s))
 	}
+}
+
+func (c *Compiler) compileReturn(node *ast.Return) error {
+	if err := c.CompileExpression(node.Value); err != nil {
+		return err
+	}
+
+	c.push(bytecode.Return)
+
+	return nil
 }
