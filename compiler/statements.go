c7911c4048839fabe0de740ef656e981d8603ea9
Zac Garby <me@zacgarby.co.uk>
2018-05-27 11:41:43 +0100

    Compile next and break

--- compiler/statements.go
+++ compiler/statements.go
@@ -15,6 +15,10 @@ func (c *Compiler) CompileStatement(s ast.Statement) error {
 		return c.CompileExpression(node.Expr)
 	case *ast.Return:
 		return c.compileReturn(node)
+	case *ast.Next:
+		return c.compileNext(node)
+	case *ast.Break:
+		return c.compileBreak(node)
 	default:
 		return fmt.Errorf("compiler: compilation not yet implemented for %s", reflect.TypeOf(s))
 	}
@@ -27,5 +31,15 @@ func (c *Compiler) compileReturn(node *ast.Return) error {
 
 	c.push(bytecode.Return)
 
+	return nil
+}
+
+func (c *Compiler) compileNext(node *ast.Next) error {
+	c.push(bytecode.Next)
+	return nil
+}
+
+func (c *Compiler) compileBreak(node *ast.Break) error {
+	c.push(bytecode.Break)
 	return nil
 }
