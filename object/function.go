b7cc6817228b29db4441fb792593d1f558e40cc6
Zac Garby <me@zacgarby.co.uk>
2018-06-18 17:04:19 +0100

    Stop gofmt whinging

--- object/function.go
+++ object/function.go
@@ -22,9 +22,9 @@ type Function struct {
 func (f *Function) String() string {
 	if f.IsMethod() {
 		return fmt.Sprintf("<method (%d) %p>", len(f.Parameters), f.Self)
-	} else {
-		return fmt.Sprintf("<function (%d)>", len(f.Parameters))
 	}
+
+	return fmt.Sprintf("<function (%d)>", len(f.Parameters))
 }
 
 // Type returns the type of an Object.
