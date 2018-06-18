b60712848a174da3a766f27a11af4d408e38f213
Zac Garby <me@zacgarby.co.uk>
2018-06-18 17:16:16 +0100

    Define the Builtin type

---
+++ object/builtin.go
@@ -0,0 +1,29 @@
+package object
+
+import (
+	"fmt"
+)
+
+// A Builtin is a function which has been written in Go but is callable from
+// a Radon program.
+type Builtin struct {
+	defaults
+	Name string
+	Fn   func(args ...Object)
+}
+
+func (b *Builtin) String() string {
+	return fmt.Sprintf("<builtin %s>", b.Name)
+}
+
+// Equals checks if two builtins are equal to each-other. Two builtins are equal
+// if their names are the same.
+func (b *Builtin) Equals(other Object) bool {
+	switch o := other.(type) {
+	case *Builtin:
+		return b.Name == o.Name
+
+	default:
+		return false
+	}
+}
